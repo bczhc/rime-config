4f0daa3e4683482f68b59d19555b875dee202b38
bczhc <bczhc0@126.com>
2023-02-08 13:47:31 +0800

    重新配置虎码，合并单虎与词版，移除秃秃的说明文件

--- rime.lua
+++ rime.lua
@@ -1,5 +1,5 @@
 charset_comment_filter = require("charset_comment_filter") --Unicode分区提示
-core2022 = require("core2022_filter") --自定义字符集过滤（常用字集）
+tiger_core2022 = require("tiger_core2022_filter") --自定义字符集过滤（常用字集）
 dz_ci = require("dz_ci_filter") --单字模式 这个别用，有问题的
 number_translator = require("number")
 lua_unicode_display_filter = require("unicode_display")  --Unicode编码提示

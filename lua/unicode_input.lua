21e6c6ca548a9cbf525a0f631db24368aeb21c3e
bczhc <bczhc0@126.com>
2023-02-10 18:38:36 +0800

    fix: crash because `utf8.char` produces non-UTF8-encoded bytes
    when codepoint is greater than 0x10FFFF

--- lua/unicode_input.lua
+++ lua/unicode_input.lua
@@ -9,7 +9,7 @@ local function translator(input, seg)
     end
 
     local codepoint = tonumber(unicode, 16)
-    if codepoint == nil then
+    if codepoint == nil or not (codepoint >= 0 and codepoint <= 0x10FFFF) then
         return
     end
     local char = utf8.char(codepoint)

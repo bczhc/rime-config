8bdbba6d4f7c4bfbad2aa09cf0ac153f4e505eef
bczhc <bczhc0@126.com>
2023-02-21 23:12:33 +0800

    候复快符要抬起才会输入
    因为key按下事件会被`recognizer`拦截掉，
    只能收到key release。所以要做一大堆
    麻烦事才能做到预期结果。
    
    本来只用`auto_select_pattern`就行了，
    但不知为什么这个东西不起作用。结果
    librime-lua一把棱，搞麻烦多了。

--- lua/short_punct_pre_speller_processor.lua
+++ lua/short_punct_pre_speller_processor.lua
@@ -1,3 +1,5 @@
+local punct_db
+
 local function processor_pre_speller(key, env)
     local repr = key:repr()
     local context = env.engine.context
@@ -13,14 +15,24 @@ local function processor_pre_speller(key, env)
     return kNoop
 end
 
-local function processor_post_speller(key, env)
+local function processor_pre_recognizer(key, env)
     local context = env.engine.context
-    if context.input:match('^\'[a-z]$') then
-        context:commit()
+    if context.input == '\'' and key:repr():match('^[a-z]$') then
+        local punct = punct_db:lookup(key:repr())
+        if punct == nil then
+            return kNoop
+        end
+        env.engine:commit_text(punct)
+        context.input = ''
         return kAccepted
     end
     return kNoop
 end
 
 _G.short_punct_pre_speller_processor = processor_pre_speller
-_G.short_punct_post_speller_processor = processor_post_speller
+_G.short_punct_pre_recognizer_processor = {
+    init = function(env)
+        punct_db = ReverseDb("build/short_punct.reverse.bin")
+    end,
+    func = processor_pre_recognizer
+}

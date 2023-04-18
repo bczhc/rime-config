f3b8a6d4057bf9ae377abc30f75e7791d16e1184
bczhc <bczhc0@126.com>
2023-04-18 17:46:51 +0800

    用lua实时反查
    doing so instead of using OpenCC can
    keep synchronized with the dictionary

---
+++ lua/092chaifen.lua
@@ -0,0 +1,27 @@
+local function filter(input, env)
+    local context = env.engine.context
+    local switch = context:get_option("chaifen")
+
+    for cand in input:iter() do
+        if switch then
+            local text = cand.text
+            for char in unicode_chars(text) do
+                local chaifen_lookup = _G._092chaifen_db:lookup(char)
+                local code_lookup = _G._092reverse_db:lookup(char)
+                local comment = '[' .. chaifen_lookup .. '·' .. code_lookup .. ']'
+                cand:get_genuine().comment = cand.comment .. ' ' .. comment
+            end
+        end
+        yield(cand)
+    end
+end
+
+return {
+    init = function(_)
+        _G._092chaifen_db = ReverseDb("build/092chaifen.reverse.bin")
+        _G._092reverse_db = ReverseDb("build/092wubi.reverse.bin")
+    end,
+    func = filter
+}
+
+
--- lua/global.lua
+++ lua/global.lua
@@ -3,3 +3,20 @@ _G.kRejected, _G.kAccepted, _G.kNoop = 0, 1, 2
 require('lib')
 
 ENABLE_MY_LOG = true
+
+function table_to_iter(table)
+    local i = 0
+    return function()
+        i = i + 1
+        return table[i]
+    end
+end
+
+function unicode_chars(text)
+    local table = {}
+    for _, cp in utf8.codes(text) do
+        local c = utf8.char(cp)
+        table[#table + 1] = c
+    end
+    return table_to_iter(table)
+end

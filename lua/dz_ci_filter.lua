e2ad015068a8f177e97ea9e89cff52b643dbb6ee
Zhai Can <bczhc0@126.com>
2025-01-05 20:34:01 +0800

    update

--- lua/dz_ci_filter.lua
+++ lua/dz_ci_filter.lua
@@ -1,9 +1,9 @@
 --    https://github.com/rime/librime/issues/248#issuecomment-468924677
 --    單字和字詞切換
 local function filter(input, env)
-    b = env.engine.context:get_option("dz_ci")
+    local enabled = env.engine.context:get_option("dz_ci")
     for cand in input:iter() do
-        if (not b or utf8.len(cand.text) == 1) then
+        if (not enabled or utf8.len(cand.text) == 1 or cand.type == "custom" or cand.type == "date") then
             yield(cand)
         end
     end
